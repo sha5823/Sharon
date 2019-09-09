e420e8cbb7a76130f26eed68d35ccf219c432b21
leo45v <leonardovv_7@hotmail.com>
2019-09-08 23:37:44 -0400

    prueba de bug

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,12 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+		Integer num1,num2;
+		num1=3;
+		num2=3;
+		if(num1==num2){
+			System.out.println("pepe");
+		}
 		System.out.print("hola");
 	}
 
